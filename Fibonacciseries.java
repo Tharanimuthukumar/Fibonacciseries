a10f857a7500e6147bb48cad6ab2aa1e3ad6918c
Tharanimuthukumar <tharani10042007@gmail.com>
2026-02-20 12:30:09 +0530

    Update Fibonacciseries.java

--- Fibonacciseries.java
+++ Fibonacciseries.java
@@ -1,21 +1,25 @@
 public class Fibonacci {
 
-    // Recursive method
+    // Recursive Method
     static int fib(int n) {
         if (n <= 1)
             return n;
-        else
-            return fib(n - 1) + fib(n - 2);
+        return fib(n - 1) + fib(n - 2);
     }
 
     public static void main(String[] args) {
 
         if (args.length < 1) {
-            System.out.println("Please provide a number.");
+            System.out.println("Please provide n value");
             return;
         }
 
         int n = Integer.parseInt(args[0]);
+
+        if (n < 0) {
+            System.out.println("n must be non-negative");
+            return;
+        }
 
         int result = fib(n);
 
